ded5d6e612c5e91cc2d3a73315afe723997fb676
Michael's <mjlikre@gmail.com>
2019-05-22 20:56:50 -0700

    done with the JS logic, done with css styling, need to add some comments for clearer logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,14 +7,96 @@ var blanksAndSuccess=[];
 var wrongLetters=[];
 var win = 0;
 var loss = 0;
-var guessLeft=9;
+var guessLeft=15;
+var lettersGuessed = '';
 
 var start = function(){
-    charChosen = marvelChar[Math.floor(Math.random()*marvelChar.length)]
-    console.log(charChosen);
+
+
+    charChosen = marvelChar[Math.floor(Math.random()*marvelChar.length)];
+    lettersInChar = charChosen.split('');
+    numBlanks= lettersInChar.length;
+
+
+    guessLeft= 15;
+    wrongLetters=[];
+    blanksAndSuccess = [];
+
+    for (var i =0; i < numBlanks; i++){
+        blanksAndSuccess.push("_")
+    }
+
+    document.getElementById("blanks").innerHTML = blanksAndSuccess.join(" ");
+    document.getElementById('guessesLeft').innerHTML = guessLeft;
+    document.getElementById('winCounter').innerHTML = win;
+    document.getElementById('lossCounter').innerHTML = loss; 
+
+   
+}
+
+
+
+
+var checker = function(letter){
+    var letterFound = false;
+    for (var i = 0; i< numBlanks; i++){
+        if (lettersInChar[i] === letter){
+            letterFound = true;
+        }
+    }
+
+    if (letterFound){
+        for (var i = 0; i < lettersInChar.length; i++){
+            if (lettersInChar[i] === letter){
+                blanksAndSuccess[i] = letter;
+            }
+        }
+
+    }
+    else{
+        wrongLetters.push(letter);
+        guessLeft --; 
+    }
+    
+    
+
+}
+
+var roundComplete = function(){
+    console.log("Win Count: " + win + "| Loss Count: " + loss + "| Guesses Left: " + guessLeft);
+
+    document.getElementById("guessesLeft").innerHTML = guessLeft;
+    document.getElementById("blanks").innerHTML = blanksAndSuccess.join(" ");
+    document.getElementById("wrongLetters").innerHTML = wrongLetters.join(" ");
+
+    
+   
+    if (lettersInChar.toString()===blanksAndSuccess.toString()){
+        win++;
+        alert("You Won!");
+
+        document.getElementById("winCounter").innerHTML = win;
+        start();
+    }
+    else if(guessLeft == 0){
+        loss ++;
+        alert("You Lost!");
+
+        document.getElementById("lossCounter").innerHTML = loss;
+        start();
+    }
+
+}
+
+document.onkeyup=function(event){
+    lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checker(lettersGuessed); 
+    roundComplete();
 }
 
 start();
+
+
 
 
 
